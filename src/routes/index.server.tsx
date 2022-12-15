f6f8d8e20b2d6a182d2673160b3f354f0dfe5b1e
johncraigcole <johncraigcole@gmail.com>
2022-12-15 11:12:24 -0500

    removed isBrowser and suspense

--- src/routes/index.server.tsx
+++ src/routes/index.server.tsx
@@ -1,10 +1,9 @@
-import { isBrowser } from "@shopify/hydrogen";
-import { Suspense } from "react";
+
 import {Animation } from "../components/Animation.client";
 
 export default function Home() {
 
 
-    return <Suspense><Animation/></Suspense>
+    return <Animation/>
 
 }
